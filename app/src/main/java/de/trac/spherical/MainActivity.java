57b3068e703e0d2606709ddbcbc851ce5445a559
vanitasvitae <vanitasvitae@fsfe.org>
2017-09-12 18:11:58 +0200

    Fix intent filters

--- app/src/main/java/de/trac/spherical/MainActivity.java
+++ app/src/main/java/de/trac/spherical/MainActivity.java
@@ -1,13 +1,23 @@
 package de.trac.spherical;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Spherical";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        Log.d(TAG, "Intent: " + action + " " + type);
     }
 }
